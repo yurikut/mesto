79df9c5f2f2d2f3039c5e9236a6faff5df47f868
Yuriy Kutakov <yurikut@yandex.ru>
2021-08-22 02:54:08 +0300

    validation index.html, index.css

--- scripts/validate.js
+++ scripts/validate.js
@@ -66,7 +66,7 @@ enableValidation({
   formSelector: '.form',
   inputSelector: '.form__input',
   submitButtonSelector: '.form__submit-button',
-  inactiveButtonClass: '.form__submit-button_disabled',
-  inputErrorClass: '.form__input_type_error',
-  errorClass: '.form__input-error_visible',
+  inactiveButtonClass: 'form__submit-button_disabled',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_visible',
 })
