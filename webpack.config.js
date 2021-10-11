a21b11b62f27adbc63945422c755b4926459c62d
Yuriy Kutakov <yurikut@yandex.ru>
2021-10-11 05:38:47 +0300

    webpack beta2

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,55 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: "./src/index.js" },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-    publicPath: "",
+  entry: {
+    main: './src/pages/index.js'
   },
-  mode: "development",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "./dist"),
-    },
+      directory: path.resolve(__dirname, './dist')
+  },
     compress: true,
     port: 8080,
-    open: true,
+    open: true
   },
-
   module: {
-    rules: [
-      // rules — это массив правил
-      // добавим в него объект правил для бабеля
-      {
-        // регулярное выражение, которое ищет все js файлы
+    rules: [{
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
-        use: "babel-loader",
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
-        exclude: "/node_modules/",
+        use: 'babel-loader',
+        exclude: '/node_modules/'
       },
-      // добавили правило для обработки файлов
       {
-        // регулярное выражение, которое ищет все файлы с такими расширениями
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
-        // применять это правило только к CSS-файлам
         test: /\.css$/,
-        // при обработке этих файлов нужно использовать
-        // MiniCssExtractPlugin.loader и css-loader
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
+        use: [MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
           },
-        ],
+          'postcss-loader'
+        ]
       },
-    ],
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html", // путь к файлу index.html
+      template: './src/index.html'
     }),
-
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
-  ],
-};
+    new MiniCssExtractPlugin(),
+
+  ]
+}
