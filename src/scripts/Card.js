ded2f58d3783c2fb9fe767f76b050fc5ac59d8ad
Yuriy Kutakov <yurikut@yandex.ru>
2021-10-12 20:42:21 +0300

    доработка существующих классов

--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -1,9 +1,15 @@
 export default class Card {
-  constructor({ link, name }, formSelector, handleCardClick) {
+  constructor({ link, name, likes, isMy, ownerId, isLike }, formSelector, handleCardClick, handleDeleteCardClick, changeLikeIcon) {
     this._name = name;
     this._link = link;
+    this.like = likes.length;
+    this.isLike = isLike;
+    this._isMy = isMy;
+    this._ownerId = ownerId; 
     this._formSelector = formSelector;
     this._handleCardClick = handleCardClick;
+    this._handleDeleteCardClick = handleDeleteCardClick;
+    this._changeLikeIcon = changeLikeIcon;
   }
 
   _getTemplate() {
@@ -22,12 +28,24 @@ export default class Card {
     this._cardImage.src = this._link;
     this._cardImage.alt = "На карточке изображено: " + this._name;
     this._likeButton = this._element.querySelector(".photos__card-like-button");
+    this._toggleLikeIcon();
+    if (this._isMy) {
+      this._element.querySelector(".photos__card-delete-button")
+      .classList.remove(".photos__card-delete-button_hidden");
+    }
+
     return this._element;
   }
 
-  _likeIcon() {
-    this._likeButton.classList.toggle("photos__card-like-button_liked");
+  likeIcon() {
+    if (this.isLike) {
+      likeButton.classList.add("photoscard-like-button_liked");
+    } else {
+      likeButton.classList.remove("photos__card-like-button_liked");
+    }
+    this._element.querySelector(".photos__card-like-count").textContent = this.like;
   }
+
 
   _deleteCard() {
     this._element.remove();
@@ -38,14 +56,16 @@ export default class Card {
     this._element
       .querySelector(".photos__card-like-button")
       .addEventListener("click", () => {
-        this._likeIcon();
+        this._changeLikeIcon(this.ownerId, this.isLike);
       });
 
-    this._element
+    if (this._isMy) {
+      this._element
       .querySelector(".photos__card-delete-button")
       .addEventListener("click", () => {
         this._deleteCard();
       });
+    }
 
     this._element
       .querySelector(".photos__card-image")
--- src/scripts/PopupWithForm.js
+++ src/scripts/PopupWithForm.js
@@ -4,7 +4,17 @@ export default class PopupWithForm extends Popup {
   constructor(popupSelector, submitForm) {
     super(popupSelector);
     this._submitForm = submitForm;
-    this._formElement = this._popup.querySelector(".form");
+    this._form = this._popup.querySelector(".form");
+    this._submitButton = this._form.querySelector("form__submit-button");
+    this._defaultValueSubmitButton = this._submitButton.textContent;
+  }
+
+  renderLoading(isLoading, loadMessage = 'Cохранение...') {
+    if (isLoading) {
+      this._submitButton.textContent = loadMessage;
+    } else {
+      this._submitButton.textContent = this._defaultValueSubmitButton;
+    }
   }
 
   getInputValues() {
@@ -18,12 +28,12 @@ export default class PopupWithForm extends Popup {
 
   close() {
     super.close();
-    this._formElement.reset();
+    this._form.reset();
   }
 
   setEventListeners() {
     super.setEventListeners();
-    this._formElement.addEventListener("submit", (evt) => {
+    this._form.addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._submitForm();
     });
