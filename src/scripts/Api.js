57e454a4935468f83ca3984639ff2dc76859d2bd
Yuriy Kutakov <yurikut@yandex.ru>
2021-11-17 02:51:27 +0300

    fix Api

--- src/scripts/Api.js
+++ src/scripts/Api.js
@@ -1,14 +1,14 @@
 export default class Api {
 
-    constructor({ url, tocken, cohortId }) {
+    constructor({ url, token, cohortId }) {
         this._cohortId = cohortId;
-        this._headers.autorization = tocken;
+        this._token = token;
         this._url = url;
     }
 
     getUserInfo() {
         return fetch(`${this._url}/${this._cohortId}/users/me`, {
-          headers: this._headers,
+          headers: this._token,
         }).then((result) => {
           return  result.ok ? result.json() : Promise.reject(`Не получены данные профиля: ${result.status} `);
         });
